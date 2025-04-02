2fb8412f995b898cfedad527d1579ce2b63b317e
sadness333 <130636434+sadness333@users.noreply.github.com>
2025-04-02 03:34:44 +0300

    fix app.py

--- app.py
+++ app.py
@@ -33,9 +33,18 @@ STATUS_COLORS = {
     'canceled': 'dark'
 }
 
-# Initialize Firebase with environment variables
-cred = credentials.Certificate("mypolka-4cf39-firebase-adminsdk-8s6z1-b700ab00cb.json")  # Укажи правильный путь
-
+cred = credentials.Certificate({
+    'type': os.getenv('FIREBASE_TYPE'),
+    'project_id': os.getenv('FIREBASE_PROJECT_ID'),
+    'private_key_id': os.getenv('FIREBASE_PRIVATE_KEY_ID'),
+    'private_key': os.getenv('FIREBASE_PRIVATE_KEY'),
+    'client_email': os.getenv('FIREBASE_CLIENT_EMAIL'),
+    'client_id': os.getenv('FIREBASE_CLIENT_ID'),
+    'auth_uri': os.getenv('FIREBASE_AUTH_URI'),
+    'token_uri': os.getenv('FIREBASE_TOKEN_URI'),
+    'auth_provider_x509_cert_url': os.getenv('FIREBASE_AUTH_PROVIDER_X509_CERT_URL'),
+    'client_x509_cert_url': os.getenv('FIREBASE_CLIENT_X509_CERT_URL')
+})
 
 firebase_admin.initialize_app(cred, {
     'databaseURL': os.getenv('FIREBASE_DATABASE_URL'),
